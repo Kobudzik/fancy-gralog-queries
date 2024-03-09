2f06837bb069f4fe5593cab24d389e56a1bfcab1
Kobu <valky143@gmail.com>
2024-03-09 11:54:38 +0100

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import QueryGenerator from "./components/QueryGenerator/QueryGenerator.js";
+import * as myScripts from "./scripts/myScripts.js";
+
+function App() {
+  myScripts.DoTheThang();
+  return (
+    <div className="App">
+      <QueryGenerator />
+    </div>
+  );
+}
+
+export default App;
